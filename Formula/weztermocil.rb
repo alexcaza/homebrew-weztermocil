24aa199819a0168b4d4fa9d2a78b1ad1005a646e
Alex Caza <alex@alexcaza.com>
2024-03-26 15:41:43 -0400

    Initial commit

---
+++ Formula/weztermocil.rb
@@ -0,0 +1,16 @@
+class Weztermocil < Formula
+  desc "Automate opening windows and panes with commands in Wezterm (like Teamocil or iTermocil)."
+  homepage "https://github.com/alexcaza/weztermocil"
+  url "https://github.com/alexcaza/weztermocil/archive/main.tar.gz"
+  license "MIT"
+  head "https://github.com/alexcaza/weztermocil.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    # generate_completions_from_executable(bin/"weztermocil", "--list", base_name: "weztermocil", shell_parameter_format: "complete-")
+    # (man1/"rg.1").write Utils.safe_popen_read(bin/"rg", "--generate", "man")
+  end
+end
